5206470bad161da37c7ef3c6498a516edbef3212
unknown <ejames.hegina@gmail.com>
2022-03-04 14:36:23 +0800

    added history

---
+++ tictactoe/js/index.js
@@ -0,0 +1,200 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const chooseSideElement = document.getElementById('chooseSide')
+var turnIndicator = document.querySelector('[data-turn]')
+var historyCount = 1
+var historyContent  = document.querySelector('[data-history-display]')
+var Xscore = 0
+var Oscore = 0
+let circleTurn
+var moveHistoryArray = []
+var nextMoveHistory = []
+
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function chooseYourSide(){
+    chooseSideElement.classList.add('show')
+    var playerX = document.getElementById('player-x')
+    var playerO = document.getElementById('player-o')
+    playerX.addEventListener('click',()=>{{
+      circleTurn = 'X'
+      console.log(circleTurn)
+      startGame()
+      chooseSideElement.classList.remove('show')
+    }})
+    playerO.addEventListener('click',()=>{
+      circleTurn = 'O'
+      console.log(circleTurn)
+      startGame()
+      chooseSideElement.classList.remove('show')
+    })
+}
+
+function startGame() {
+  chooseYourSide()
+  displayScore()
+  showTurn()
+  clearHistory()
+  if(circleTurn == 'X'){
+    circleTurn = false
+  }else{
+    circleTurn = true
+  }
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
+}
+
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+    turnIndicator.textContent = ""
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    circleTurn ? Oscore += 1 : Xscore += 1
+    console.log(`Xscore: ${Xscore} \n O Score: ${Oscore}`)
+    turnIndicator.textContent = ""
+  }
+  winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+  console.log(`cell: ${cell.innerText} current Class: ${currentClass}`)
+  showTurn()
+  //Record Move
+  moveHistoryArray.push([cell.innerText,currentClass])
+  console.log(moveHistoryArray)
+  //Add history Function
+  if(currentClass == 'x'){
+    currentClass = 'X'
+  }else{
+    currentClass = 'O'
+  }
+  var pTag = document.createElement('p')
+  var moveContent = document.createTextNode(`${historyCount}.)  player ${currentClass} move in tile ${cell.innerText}`)
+  pTag.appendChild(moveContent)
+  historyContent.appendChild(pTag)
+  historyCount += 1
+
+}
+function showTurn(){
+  if(circleTurn == true){
+    turnIndicator.textContent = `X's turn`
+  }else if(circleTurn == false){
+    turnIndicator.textContent = `O's turn`
+  }else if(circleTurn == undefined){
+    turnIndicator.textContent = ""
+  }else{
+    turnIndicator.textContent = `${circleTurn}'s turn`
+  }
+  
+}
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
+
+function displayScore(){
+  document.querySelector('[data-x-score]').textContent = Xscore
+  document.querySelector('[data-o-score]').textContent = Oscore
+}
+
+// HISTORY
+
+function clearHistory(){
+  historyContent.innerHTML = ""
+  historyCount =1
+}
+
+function selectChild(){
+  
+  var currentCell = document.getElementsByClassName('cell')[0].textContent
+  console.log(currentCell)
+}
+selectChild()
+//Previous
+
+const btnPrev = document.getElementById('btnPreviousMove')
+function btnDisabled(){
+  if(moveHistoryArray.length <= 0){
+    btnPrev.disabled = true
+  }else{
+    btnPrev.disabled = false
+  }
+}
+
+btnPrev.addEventListener('click',prevMove)
+function prevMove(){
+  if(moveHistoryArray.length <= 0){
+    alert(`No more previous move`)
+  }else{
+    let getLastMove = moveHistoryArray.pop()
+    nextMoveHistory.push(getLastMove)
+    let nextArrayLength = nextMoveHistory.length - 1
+    let childIndex = parseInt(getLastMove[nextArrayLength][0]) -1
+    let getclass = getLastMove[nextArrayLength][1]
+    console.log(nextArrayLength)
+    console.log(childIndex)
+    document.getElementsByClassName('cell')[childIndex].classList.remove(getclass)
+  }
+}
